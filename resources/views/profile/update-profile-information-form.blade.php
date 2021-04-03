6253d0c077e4d3caa486e9b0a75dd390acf7265d
Isaac <izac3d@gmail.com>
2021-04-03 18:04:05 +0200

    added phone and job title

---
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -0,0 +1,123 @@
+<x-jet-form-section submit="updateProfileInformation">
+    <x-slot name="title">
+        {{ __('Profile Information') }}
+    </x-slot>
+
+    <x-slot name="description">
+        {{ __('Update your account\'s profile information and email address.') }}
+    </x-slot>
+
+    <x-slot name="form">
+
+        <x-jet-action-message on="saved">
+            {{ __('Saved.') }}
+        </x-jet-action-message>
+
+        <!-- Profile Photo -->
+        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+            <div class="form-group" x-data="{photoName: null, photoPreview: null}">
+                <!-- Profile Photo File Input -->
+                <input type="file" hidden
+                       wire:model="photo"
+                       x-ref="photo"
+                       x-on:change="
+                                    photoName = $refs.photo.files[0].name;
+                                    const reader = new FileReader();
+                                    reader.onload = (e) => {
+                                        photoPreview = e.target.result;
+                                    };
+                                    reader.readAsDataURL($refs.photo.files[0]);
+                            " />
+
+                <x-jet-label for="photo" value="{{ __('Photo') }}" />
+
+                <!-- Current Profile Photo -->
+                <div class="mt-2" x-show="! photoPreview">
+                    <img src="{{ $this->user->profile_photo_url }}" class="rounded-circle" height="80px" width="80px">
+                </div>
+
+                <!-- New Profile Photo Preview -->
+                <div class="mt-2" x-show="photoPreview">
+                    <img x-bind:src="photoPreview" class="rounded-circle" width="80px" height="80px">
+                </div>
+
+                <x-jet-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
+                    {{ __('Select A New Photo') }}
+				</x-jet-secondary-button>
+
+				@if ($this->user->profile_photo_path)
+                    <x-jet-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
+                        {{ __('Remove Photo') }}
+                    </x-jet-secondary-button>
+                @endif
+
+                <x-jet-input-error for="photo" class="mt-2" />
+            </div>
+        @endif
+
+        <div class="row w-md-100">
+            <!-- Name -->
+            <div class="form-group col-md-6">
+                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-input id="name" type="text" class="{{ $errors->has('name') ? 'is-invalid' : '' }}" wire:model.defer="state.name" autocomplete="name" />
+                <x-jet-input-error for="name" />
+            </div>
+
+            <!-- Email -->
+            <div class="form-group col-md-6">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" type="email" class="{{ $errors->has('email') ? 'is-invalid' : '' }}" wire:model.defer="state.email" />
+                <x-jet-input-error for="email" />
+            </div>
+
+            <!-- Phone -->
+            <div class="form-group col-md-6">
+                <x-jet-label for="phone" value="{{ __('Phone') }}" />
+                <x-jet-input id="phone" type="tel" wire:model.defer="state.phone" />
+            </div>
+
+            <!-- Job Title -->
+            <div class="form-group col-md-6">
+                <x-jet-label for="job_title" value="{{ __('Job Title') }}" />
+                <x-jet-input id="jobt_itle" type="text" wire:model.defer="state.job_title" />
+            </div>
+        </div>
+        <hr class="mt-2 mb-3"/>
+        <p class="text-center">{{ __('Change Password') }}</p>
+
+        <div class="row w-md-100">
+            <div class="form-group col-md-4">
+                <x-jet-label for="current_password" value="{{ __('Current Password') }}" />
+                <x-jet-input id="current_password" type="password" class="{{ $errors->has('current_password') ? 'is-invalid' : '' }}"
+                             wire:model.defer="state.current_password" autocomplete="current-password" />
+                <x-jet-input-error for="current_password" />
+            </div>
+
+            <div class="form-group col-md-4">
+                <x-jet-label for="password" value="{{ __('New Password') }}" />
+                <x-jet-input id="password" type="password" class="{{ $errors->has('password') ? 'is-invalid' : '' }}"
+                             wire:model.defer="state.password" autocomplete="new-password" />
+                <x-jet-input-error for="password" />
+            </div>
+
+            <div class="form-group col-md-4">
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" type="password" class="{{ $errors->has('password_confirmation') ? 'is-invalid' : '' }}"
+                             wire:model.defer="state.password_confirmation" autocomplete="new-password" />
+                <x-jet-input-error for="password_confirmation" />
+            </div>
+        </div>
+
+
+    </x-slot>
+
+
+
+    <x-slot name="actions">
+		<div class="d-flex align-items-baseline">
+			<x-jet-button>
+				{{ __('Save') }}
+			</x-jet-button>
+		</div>
+    </x-slot>
+</x-jet-form-section>
---
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -0,0 +1,20 @@
+@props(['submit'])
+
+<div {{ $attributes->merge(['class' => 'row']) }}>
+
+    <div class="col-md-12">
+        <div class="card shadow-sm">
+            <form wire:submit.prevent="{{ $submit }}">
+                <div class="card-body">
+                {{ $form }}
+                </div>
+
+                @if (isset($actions))
+                    <div class="card-footer d-flex justify-content-end">
+                        {{ $actions }}
+                    </div>
+                @endif
+            </form>
+        </div>
+    </div>
+</div>
